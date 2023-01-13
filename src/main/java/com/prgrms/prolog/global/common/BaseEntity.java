44f3fc002e03b729432b6e43dcf566e334caab80
kwonjoosung <kwonjoosung@gmail.com>
2023-01-13 17:13:54 +0900

    feat: BaseEntity 구현
    - CreateBy: 시큐리티 연동 X (TODO)
    - 생성일자, 수정일자, 작성자

---
+++ src/main/java/com/prgrms/prolog/global/common/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.prgrms.prolog.global.common;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Column(name = "created_by")
+	private Long createdBy;
+}
