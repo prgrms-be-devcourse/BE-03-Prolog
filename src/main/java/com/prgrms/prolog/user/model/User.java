f4e32d58c898c062b4b5a71f3259073cc9d81192
kwonjoosung <kwonjoosung@gmail.com>
2023-01-13 18:12:13 +0900

    refactor: PR 피드백 반영
    - Regex(정규식) static으로
    - @Colum -> @Size로 변경

--- src/main/java/com/prgrms/prolog/user/model/User.java
+++ src/main/java/com/prgrms/prolog/user/model/User.java
@@ -3,12 +3,12 @@ package com.prgrms.prolog.user.model;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 import org.springframework.util.Assert;
 
@@ -27,26 +27,29 @@ import lombok.extern.slf4j.Slf4j;
 @Table(name = "users")
 public class User extends BaseEntity {
 
+	private static final Pattern emailPattern
+		= Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String email;
 
-	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String nickName;
 
-	@Column(nullable = true, length = 100)
+	@Size(max = 100)
 	private String introduce;
 
-	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String prologName;
 
-	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String provider;
 
-	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String oauthId;
 
 	@Builder
@@ -81,10 +84,7 @@ public class User extends BaseEntity {
 	}
 
 	private void validateEmail(String email) {
-		String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(email);
-
+		Matcher matcher = emailPattern.matcher(email);
 		Assert.hasText(email, "이메일은 빈 값일 수 없습니다.");
 		Assert.isTrue(matcher.matches(), "이메일 형식이 올바르지 않습니다.");
 		checkOverLength(email, 100, "이메일은 최대 100글자 이내로 작성해야 합니다..");
@@ -99,14 +99,14 @@ public class User extends BaseEntity {
 
 	@Override
 	public String toString() {
-		return "User{" +
-			"id=" + id +
-			", email='" + email +
-			", nickName='" + nickName +
-			", introduce='" + introduce +
-			", prologName='" + prologName +
-			", provider='" + provider +
-			", oauthId='" + oauthId +
-			'}';
+		return "User{"
+			+ "id=" + id
+			+ ", email='" + email
+			+ ", nickName='" + nickName
+			+ ", introduce='" + introduce
+			+ ", prologName='" + prologName
+			+ ", provider='" + provider
+			+ ", oauthId='" + oauthId
+			+ '}';
 	}
 }
