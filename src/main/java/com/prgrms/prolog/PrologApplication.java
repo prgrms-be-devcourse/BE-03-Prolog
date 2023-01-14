6442effc18aff4a9134012312612afce99387eb7
hyunseo <goatsfish@gmail.com>
2023-01-14 21:21:53 +0900

    add: Aspectj auto proxy 어노테이션 추가

--- src/main/java/com/prgrms/prolog/PrologApplication.java
+++ src/main/java/com/prgrms/prolog/PrologApplication.java
@@ -2,7 +2,9 @@ package com.prgrms.prolog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class PrologApplication {
 
