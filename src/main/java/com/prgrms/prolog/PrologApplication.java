dcee2982c7fae8681210990e4ce3a15126dc22d2
hyunseo <goatsfish@gmail.com>
2023-01-16 16:35:00 +0900

    update: AOP 설정 삭제
    - gradle에 AOP 의존을 삭제한다.
    - main Application에 AOP 어노테이션을 삭제한다.

--- src/main/java/com/prgrms/prolog/PrologApplication.java
+++ src/main/java/com/prgrms/prolog/PrologApplication.java
@@ -2,9 +2,7 @@ package com.prgrms.prolog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class PrologApplication {
 
