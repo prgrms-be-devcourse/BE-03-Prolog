e6cc633ab5d39bf64594c85fbaf03e30c6930668
kwonjoosung <kwonjoosung@gmail.com>
2023-01-18 17:54:51 +0900

    refactor: JPA auditing 설정 변경
    -

--- src/main/java/com/prgrms/prolog/PrologApplication.java
+++ src/main/java/com/prgrms/prolog/PrologApplication.java
@@ -2,9 +2,7 @@ package com.prgrms.prolog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class PrologApplication {
 
