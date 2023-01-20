afcc1e398d2041eaf7536ac17e380ce31733ebbb
Fortune00 <53924962+Sinyoung3016@users.noreply.github.com>
2023-01-20 19:12:49 +0900

    [#46] db 설정을 외부에서 설정할 수 있도록 환경 변수로 분리하기 (#51)
    * add: docker 실행 시, env를 외부 입력할 수 있도록 수정
    
    - action 실행 분기 수정, 너무 불필요하게 많이 실행되는 것 같아서 push 시에만 실행되도록 반영
    - 배포 워크플로우, build 전에 env 파일 만들어야 빌드 가능해서 생성하는 코드 추가
    
    * update: 문법 수정
    
    * update: profile 설정
    
    * update: application-db 수정
    
    * update: 배포 분기 ** 에서 main으로 변경

--- src/main/java/com/prgrms/prolog/global/config/DatabaseConfig.java
+++ src/main/java/com/prgrms/prolog/global/config/DatabaseConfig.java
@@ -1,9 +1,11 @@
 package com.prgrms.prolog.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Profile("local")
 @PropertySource("classpath:db/db.properties") // env(db).properties 파일 소스 등록
 public class DatabaseConfig {
 
