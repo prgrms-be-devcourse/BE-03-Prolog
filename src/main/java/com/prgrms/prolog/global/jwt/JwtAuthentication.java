6c0bfdd44def512a0530595b54d4bb10196bbaed
kwonjoosung <kwonjoosung@gmail.com>
2023-01-24 20:34:15 +0900

    refactor: Jwt Payload(Claim)를 email에서 id로 변경

--- src/main/java/com/prgrms/prolog/global/jwt/JwtAuthentication.java
+++ src/main/java/com/prgrms/prolog/global/jwt/JwtAuthentication.java
@@ -2,13 +2,13 @@ package com.prgrms.prolog.global.jwt;
 
 import java.util.Objects;
 
-public record JwtAuthentication(String token, String userEmail) {
+public record JwtAuthentication(String token, Long id) {
 
 	public JwtAuthentication {
 		validateToken(token);
-		validateUserEmail(userEmail);
-
+		validateId(id);
 	}
+
 
 	private void validateToken(String token) {
 		if (Objects.isNull(token) || token.isBlank()) {
@@ -16,17 +16,17 @@ public record JwtAuthentication(String token, String userEmail) {
 		}
 	}
 
-	private void validateUserEmail(String userEmail) {
-		if (Objects.isNull(userEmail) || userEmail.isBlank()) {
-			throw new IllegalArgumentException("유저 이메일이 없습니다.");
+	private void validateId(Long userId) {
+		if (Objects.isNull(userId) || userId <= 0L) {
+			throw new IllegalArgumentException("유저의 ID가 없습니다.");
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "JwtAuthentication{"
-			+ "token='" + token + '\''
-			+ ", userEmail='" + userEmail + '\''
-			+ '}';
+		return "JwtAuthentication{" +
+			"token='" + token + '\'' +
+			", id='" + id + '\'' +
+			'}';
 	}
 }
