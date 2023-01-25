7d5e8c017461ec28ae23ab1e66f88e71ada31002
hyunseo <goatsfish@gmail.com>
2023-01-26 03:03:15 +0900

    refactor: 게시글 수정 메서드 리팩터링
    - 게시글 수정시 태그 업데이트 메서드 리팩터링
      - 새로운 RootTag 존재시 저장한다.
      - 없어진 PostTag를 삭제한다.
        - UserTag 카운트를 줄인다.
          - UserTag 카운트가 0일시에 삭제한다.
      - 새로운 PostTag를 저장한다.
        - UserTag를 새로 저장하거나 카운트를 증가한다.

--- src/main/java/com/prgrms/prolog/domain/post/service/PostService.java
+++ src/main/java/com/prgrms/prolog/domain/post/service/PostService.java
@@ -7,9 +7,9 @@ import com.prgrms.prolog.domain.post.dto.PostRequest;
 import com.prgrms.prolog.domain.post.dto.PostResponse;
 
 public interface PostService {
-		public Long save(PostRequest.CreateRequest request, Long userId);
-		public PostResponse findById(Long postId);
-		public Page<PostResponse> findAll(Pageable pageable);
-		public PostResponse update(PostRequest.UpdateRequest update, Long userId, Long postId);
-		public void delete(Long id);
+	Long save(PostRequest.CreateRequest request, Long userId);
+	PostResponse findById(Long postId);
+	Page<PostResponse> findAll(Pageable pageable);
+	PostResponse update(PostRequest.UpdateRequest update, Long userId, Long postId);
+	void delete(Long id);
 }
