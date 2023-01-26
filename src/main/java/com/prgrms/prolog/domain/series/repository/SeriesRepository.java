a0f6033bb1be2e95897f0cd35f71cdf4a5781be5
kwonjoosung <kwonjoosung@gmail.com>
2023-01-26 14:31:29 +0900

    update: JPQL 쿼리 변경
    -

--- src/main/java/com/prgrms/prolog/domain/series/repository/SeriesRepository.java
+++ src/main/java/com/prgrms/prolog/domain/series/repository/SeriesRepository.java
@@ -3,14 +3,19 @@ package com.prgrms.prolog.domain.series.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.prgrms.prolog.domain.series.model.Series;
 
 public interface SeriesRepository extends JpaRepository<Series, Long> {
 
-	// @Query("""
-	//	title & userId가 같은 Series, 한방에 잘 가져올 수 없나..?
-	// 	""")
-	Optional<Series> findByIdAndTitle(Long userId, String title);
+	@Query("""
+  		SELECT s
+		FROM Series s
+		WHERE s.user.id = :userId
+		and s.title = :title
+		""")
+	Optional<Series> findByIdAndTitle(@Param(value = "userId") Long userId, @Param(value = "title") String title);
 
 }
