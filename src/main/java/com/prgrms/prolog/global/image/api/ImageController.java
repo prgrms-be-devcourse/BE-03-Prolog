4533d69dcf0e67c6d4b1a2dcbdabe43403ebaebe
kwonjoosung <kwonjoosung@gmail.com>
2023-01-29 04:30:57 +0900

    update: profile 설정 변경

--- src/main/java/com/prgrms/prolog/global/image/api/ImageController.java
+++ src/main/java/com/prgrms/prolog/global/image/api/ImageController.java
@@ -1,40 +1,42 @@
 package com.prgrms.prolog.global.image.api;
 
-import com.prgrms.prolog.global.image.dto.UploadFileResponse;
-import com.prgrms.prolog.global.image.util.FileManager;
-import com.prgrms.prolog.global.image.util.UploadFileToS3;
-import lombok.RequiredArgsConstructor;
+import java.io.File;
+import java.util.UUID;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.util.UUID;
+import com.prgrms.prolog.global.image.dto.UploadFileResponse;
+import com.prgrms.prolog.global.image.util.FileManager;
+import com.prgrms.prolog.global.image.util.UploadFileToS3;
 
+import lombok.RequiredArgsConstructor;
+
+@Profile("!test")
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
 
-    private final FileManager fileManager;
-    private final UploadFileToS3 uploadFileToS3;
+	private static final String FILE_PATH = "posts";
+	private final FileManager fileManager;
+	private final UploadFileToS3 uploadFileToS3;
 
-    private static final String FILE_PATH = "posts";
+	@PostMapping("/file")
+	public UploadFileResponse uploadFile(
+		@RequestPart(value = "file") MultipartFile multipartFile
+	) {
+		String id = UUID.randomUUID().toString();
+		String originalFilename = multipartFile.getOriginalFilename();
+		String fileName = (originalFilename + id).replace(" ", "");
 
-    @PostMapping("/file")
-    public UploadFileResponse uploadFile(
-            @RequestPart(value = "file") MultipartFile multipartFile
-    ) {
-        String id = UUID.randomUUID().toString();
-        String originalFilename = multipartFile.getOriginalFilename();
-        String fileName = (originalFilename + id).replace(" ", "");
+		File tempTargetFile = fileManager.convertMultipartFileToFile(multipartFile)
+			.orElseThrow(() -> new IllegalArgumentException("exception.file.convert"));
+		String fileUrl = uploadFileToS3.upload(tempTargetFile, FILE_PATH, fileName);
 
-        File tempTargetFile = fileManager.convertMultipartFileToFile(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("exception.file.convert"));
-        String fileUrl = uploadFileToS3.upload(tempTargetFile, FILE_PATH, fileName);
-
-        fileManager.removeFile(tempTargetFile);
-        return UploadFileResponse.toUploadFileResponse(originalFilename, fileUrl);
-    }
+		fileManager.removeFile(tempTargetFile);
+		return UploadFileResponse.toUploadFileResponse(originalFilename, fileUrl);
+	}
 }
--- src/main/java/com/prgrms/prolog/global/image/util/UploadFileToS3.java
+++ src/main/java/com/prgrms/prolog/global/image/util/UploadFileToS3.java
@@ -1,25 +1,29 @@
 package com.prgrms.prolog.global.image.util;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class UploadFileToS3 {
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+	private final AmazonS3Client amazonS3Client;
 
-    private final AmazonS3Client amazonS3Client;
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
 
-    public String upload(File uploadFile, String dirName, String fileName) {
-        String savedFileName = dirName + "/" + fileName;
-        amazonS3Client.putObject(new PutObjectRequest(bucket, savedFileName, uploadFile));
-        return amazonS3Client.getUrl(bucket, savedFileName).toString();
-    }
+	public String upload(File uploadFile, String dirName, String fileName) {
+		String savedFileName = dirName + "/" + fileName;
+		amazonS3Client.putObject(new PutObjectRequest(bucket, savedFileName, uploadFile));
+		return amazonS3Client.getUrl(bucket, savedFileName).toString();
+	}
 }
