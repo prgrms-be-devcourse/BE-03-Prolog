bf1af056e63326e8a6a158b711a0e0d58aa56838
hikarigin99 <hikarigin99@gmail.com>
2023-02-01 18:36:57 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/com/prgrms/prolog/domain/series/repository/SeriesRepositoryTest.java
+++ src/test/java/com/prgrms/prolog/domain/series/repository/SeriesRepositoryTest.java
@@ -1,25 +1,15 @@
 package com.prgrms.prolog.domain.series.repository;
 
-import static com.prgrms.prolog.utils.TestUtils.*;
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.prgrms.prolog.base.RepositoryTest;
-import com.prgrms.prolog.domain.series.model.Series;
 
 public class SeriesRepositoryTest extends RepositoryTest {
 
 	@Test
 	@DisplayName("해당 유저가 가진 시리즈 중에서 찾는 제목의 시리즈를 조회한다.")
 	void findByIdAndTitleTest() {
-		// given & when
-		Optional<Series> series = seriesRepository.findByIdAndTitle(savedUser.getId(), SERIES_TITLE);
-		// then
-		assertThat(series).isPresent();
-	}
 
+	}
 }
