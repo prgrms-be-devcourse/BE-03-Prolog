d3034ef102862ccb5ba8fb3914e2a6a8b19d65de
kwonjoosung <kwonjoosung@gmail.com>
2023-02-03 15:54:17 +0900

    feat: User 업데이트 및 삭제 기능 추가
    - 이전 merge 작업에서 삭제된 부분 복구

--- src/main/java/com/prgrms/prolog/domain/user/api/UserController.java
+++ src/main/java/com/prgrms/prolog/domain/user/api/UserController.java
@@ -1,11 +1,19 @@
 package com.prgrms.prolog.domain.user.api;
 
 import static com.prgrms.prolog.domain.user.dto.UserDto.*;
+import static org.springframework.http.HttpStatus.*;
+
+import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.prolog.domain.user.service.UserService;
@@ -20,6 +28,15 @@ public class UserController {
 
 	private final UserService userService;
 
+	@GetMapping("/{userId}")
+	ResponseEntity<UserResponse> getUserProfile(
+		@PathVariable Long userId
+	) {
+		return ResponseEntity.ok(
+			userService.getUserProfile(userId)
+		);
+	}
+
 	@GetMapping("/me")
 	ResponseEntity<UserResponse> getMyProfile(
 		@AuthenticationPrincipal JwtAuthentication user
@@ -27,6 +44,24 @@ public class UserController {
 		return ResponseEntity.ok(
 			userService.getUserProfile(user.id())
 		);
+	}
+
+	@PutMapping("/me")
+	ResponseEntity<UserResponse> updateMyProfile(
+		@Valid @RequestBody UpdateUserRequest updateUserRequest,
+		@AuthenticationPrincipal JwtAuthentication user
+	) {
+		return ResponseEntity.ok(
+			userService.updateUserProfile(updateUserRequest, user.id())
+		);
+	}
+
+	@DeleteMapping("/me")
+	@ResponseStatus(NO_CONTENT)
+	void deleteMyProfile(
+		@AuthenticationPrincipal JwtAuthentication user
+	) {
+		userService.deleteUser(user.id());
 	}
 
 }
--- src/main/java/com/prgrms/prolog/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/prgrms/prolog/domain/user/service/UserServiceImpl.java
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private static final String DEFAULT_INTRODUCE = "한줄 소개를 입력해주세요.";
+	private static final String USER_NOT_FOUND_EXCEPTION = messageKey().exception().user().notExists().endKey();
 
 	private final UserRepository userRepository;
 
@@ -26,7 +27,7 @@ public class UserServiceImpl implements UserService {
 	public UserResponse getUserProfile(Long userId) {
 		return userRepository.findById(userId)
 			.map(UserResponse::from)
-			.orElseThrow(() -> new IllegalArgumentException(messageKey().exception().user().notExists().endKey()));
+			.orElseThrow(() -> new IllegalArgumentException(USER_NOT_FOUND_EXCEPTION));
 	}
 
 	/* [회원 가입] 등록된 사용자 인지 확인해서 맞는 경우 유저ID 제공, 아닌 경우 사용자 등록 */
@@ -37,6 +38,23 @@ public class UserServiceImpl implements UserService {
 			.findByProviderAndOauthId(userInfo.provider(), userInfo.oauthId())
 			.map(User::getId)
 			.orElseGet(() -> register(userInfo).getId());
+	}
+
+	/* [프로필 수정] 사용자의 프로필을 수정합니다. 값을 덮어 쓰기 때문에 수정되지 않은 내용이어도 원본 데이터가 필요합니다.*/
+	@Override
+	@Transactional
+	public UserResponse updateUserProfile(UpdateUserRequest updateUserRequest, Long userId) {
+		return userRepository.findById(userId)
+			.map(user -> user.changeUserProfile(updateUserRequest))
+			.map(UserResponse::from)
+			.orElseThrow(() -> new IllegalArgumentException("exception.user.update.fail"));
+	}
+
+	/* [사용자 삭제] 사용자를 삭제(소프트 딜리트 적용) */
+	@Override
+	@Transactional
+	public void deleteUser(Long userId) {
+		userRepository.deleteById(userId);
 	}
 
 	/* [사용자 등록] 디폴트 설정 값으로 회원가입 진행 */
